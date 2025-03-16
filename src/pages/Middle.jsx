f27300b488c3c83cac64e6c3baf2927c4daf6ffa
fed1k <allamurotovcoder@mail.ru>
2025-03-16 20:54:04 +0500

    zxc

--- src/pages/Middle.jsx
+++ src/pages/Middle.jsx
@@ -29,11 +29,11 @@ function Middle() {
 
         <p className='text-Inter font-medium text-[20px] text-center mb-[106px] md:mb-10 leading-5 text-[#C8C8C8]'>Ты ответил правильно на {result?.correct_count} вопросов и <br /> набрал {result?.score} баллов</p>
 
-        <button onClick={handleTgRedirect} className='flex gap-2 justify-center items-center mb-6 bg-[#01A3FE] p-2.5 rounded-[16px] h-14 w-[295px] md:w-[385px]'>
+        <a href="https://t.me/+TOQEscPuVNhiNWZi" target='_blank' onClick={handleTgRedirect} className='flex gap-2 justify-center items-center mb-6 bg-[#01A3FE] p-2.5 rounded-[16px] h-14 w-[295px] md:w-[385px]'>
           <span className='text-Inter text-white text-[20px] font-medium'>Наш телеграм-канал</span>
           <FaTelegramPlane className='w-5 h-5 text-white' />
-        </button>
-        <Link to="/work">
+        </a>
+        <Link to="/">
           <p className='text-Inter cursor-pointer font-medium text-[20px] text-center mb-5 leading-5 text-[#01A3FE]'>Пройти тест заново</p>
         </Link>
       </div>
